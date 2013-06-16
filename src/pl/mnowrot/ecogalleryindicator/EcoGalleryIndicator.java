9db4accb0c1b149728ab420dff2560154046de59
mnowrot <marcin.nowrot@gmail.com>
2013-06-16 19:03:36 +0200

    Basic initialization

--- src/pl/mnowrot/ecogalleryindicator/EcoGalleryIndicator.java
+++ src/pl/mnowrot/ecogalleryindicator/EcoGalleryIndicator.java
@@ -3,15 +3,22 @@
  */
 package pl.mnowrot.ecogalleryindicator;
 
+import us.feras.ecogallery.EcoGallery;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
+import android.view.View;
 
 /**
  * @author marcin
  * 
  */
-public class EcoGalleryIndicator extends ViewGroup {
+public class EcoGalleryIndicator extends View {
+
+	private EcoGallery mEcoGallery;
+	private int mColor;
+	private int mSize;
 
 	/**
 	 * @param context
@@ -25,7 +32,7 @@ public class EcoGalleryIndicator extends ViewGroup {
 	 * @param attrs
 	 */
 	public EcoGalleryIndicator(Context context, AttributeSet attrs) {
-		this(context, attrs, R.style.AppTheme);
+		this(context, attrs, 0);
 	}
 
 	/**
@@ -36,16 +43,33 @@ public class EcoGalleryIndicator extends ViewGroup {
 	public EcoGalleryIndicator(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 
+		initialize(context, attrs, defStyle);
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.view.ViewGroup#onLayout(boolean, int, int, int, int)
-	 */
+	private void initialize(Context context, AttributeSet attrs, int defStyle) {
+		TypedArray a = null;
+		try {
+			a = context.obtainStyledAttributes(attrs, R.styleable.EcoGalleryIndicator, defStyle, 0);
+			mColor = a.getColor(R.styleable.EcoGalleryIndicator_color, android.R.color.background_light);
+			mSize = a.getDimensionPixelSize(R.styleable.EcoGalleryIndicator_size, context.getResources()
+					.getDimensionPixelSize(R.dimen.ecoGallery_indicator_size_default));
+		} finally {
+			a.recycle();
+		}
+	}
+
 	@Override
-	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		// This view must not contain child views, so this is a no-op
+	protected void onDraw(Canvas canvas) {
+
+	}
+
+	public EcoGallery getEcoGallery() {
+		return mEcoGallery;
+	}
+
+	public void setEcoGallery(EcoGallery mEcoGallery) {
+		this.mEcoGallery = mEcoGallery;
 	}
 
 }
