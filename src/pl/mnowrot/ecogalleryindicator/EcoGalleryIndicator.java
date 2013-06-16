e4014924b22452096295d4227aa9dfa21df7b58f
mnowrot <marcin.nowrot@gmail.com>
2013-06-16 14:33:01 +0200

    Initial project structure

---
+++ src/pl/mnowrot/ecogalleryindicator/EcoGalleryIndicator.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package pl.mnowrot.ecogalleryindicator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * @author marcin
+ *
+ */
+public class EcoGalleryIndicator extends ViewGroup {
+
+	/**
+	 * @param context
+	 */
+	public EcoGalleryIndicator(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public EcoGalleryIndicator(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public EcoGalleryIndicator(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.view.ViewGroup#onLayout(boolean, int, int, int, int)
+	 */
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
